a072ce59a526d27cb00c7d300028b1952bccbd7f
Borislav Arnaudov <bobya@data.bg>
2012-01-08 14:25:54 +0200

    fixed task detection when running bitcoins

--- submitMaster.py
+++ submitMaster.py
@@ -49,7 +49,8 @@ class SubmitMaster(Thread):
                     execute("start")                # Start bitcoins if there is no hash to brake
                 while (self.__stopProcessing or self.pq.empty()) and  not self.__quit:
                     log.debug("Waiting for a task, or start of the execution...")
-                    time.sleep(5)               # Sleep untill 
+                    self._loadQueue()
+                    time.sleep(30)               # Sleep untill 
                 if config["executeBitCoin"]:
                     execute("stop")                 # Stop bitcoins and continue with hash tasks
             time.sleep(5)
